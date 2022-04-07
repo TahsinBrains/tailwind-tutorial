3024b2db4b6916212d750e4c53b67495ae064e93
Tahsin Ahmed <tahsin.ahmed23@brainstation-23.com>
2022-04-07 22:50:06 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: ["public/index.html"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#FF6363',
+                secondary: {
+                    100: '#00A5B2',
+                    200: '#888883',
+                }
+            },
+            fontFamily: {
+                body: ['Nunito']
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
